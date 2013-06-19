0dbde665526d7156ffe697595af2ab41db27559b
Andreas <admin@azorga.de>
2013-06-19 19:55:33 +0200

    Stub KategorieException hinzugefügt - testweise -

---
+++ src/main/java/com/haeger/time/haegertime/exception/KategorieException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.haeger.time.haegertime.exception;
+
+/**
+ *
+ * Enthaelt potenzielle Kategorie-Exceptions
+ * 
+ * @author andreas
+ */
+public class KategorieException extends Exception{
+    
+    //todo
+    
+}
